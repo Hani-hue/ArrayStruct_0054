47a83e957f62c1134acbdece672daf139baa1523
Hani-hue <isnahabibah8729@gmail.com>
2025-04-14 11:02:17 +0700

    memberi nilai pada elemen array, memberinya dengan looping dan looping for

--- Array.cpp
+++ Array.cpp
@@ -9,3 +9,29 @@ string nama[5] = {"ben", "agus", "fara", "ama", "akila"};
 
 // Mendeklarasikan array dengan ukuran dan tanpa nilai awal
 float score[5];
+
+int main()
+{
+    cout << "Nama pada index 4 adalah " << nama[4] << endl;
+    // memberi nilai pada elemen array per index
+    nama[4] = "Hani";
+    cout << "Nama pada index 4 adalah " << nama[4] << endl;
+
+    cout << "inputscore" << endl;
+    cout << endl;
+
+    // memberi nilai array dengan looping
+    for (int i = 0; i < 5; i++)
+    {
+        cout << "score ke-" << i + 1 << " =";
+        cin >> score[i];
+    }
+
+    cout << "Tampilkan skor" << endl;
+    cout << endl;
+
+    // memberi nilai array dengan looping for
+    for (int i = 0; i < 5; i++)
+    {
+    }
+}
